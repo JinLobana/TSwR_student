324979ea63a11b0c66d85c38da0e5429c44a8bf7
JinLobana <lobanajin@gmail.com>
2024-09-16 22:47:50 +0200

    added feedback linearization controller, made model just like in the urdf file

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -12,4 +12,8 @@ class FeedbackLinearizationController(Controller):
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+        v = q_r_ddot
+        tau = np.dot(self.model.M(x), v) + np.dot(self.model.C(x), q_r_dot)
+        
+
+        return tau
